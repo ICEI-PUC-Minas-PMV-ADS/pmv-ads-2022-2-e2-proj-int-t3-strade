179373a3ee021b6085726bc2db2d52853f9eea51
thassiaferr <thassiaferr@gmail.com>
2022-10-22 18:21:26 -0300

    DEV - Incrementando a funcionalidade de modo escuro

--- src/code/script/strade.js
+++ src/code/script/strade.js
@@ -8,6 +8,9 @@ window.onload = function () {
   configurarMascaraTel();
   clickCadastrar();
   desativaInputs();
+  //toggleDarkMode();
+  loadTheme();
+  clickDarkBtn();
 }
 
 function clickCadastrar(){  
@@ -234,3 +237,38 @@ function somenteNumeros(e) {
       }
   }
 }
+
+/* DARK MODE */
+loadTheme();
+
+
+
+// Toggle dark mode
+function toggleDarkMode() {
+    document.body.classList.toggle("dark");
+}
+
+// Load light or dark mode
+function loadTheme() {
+    const darkMode = localStorage.getItem("dark")
+
+    if(darkMode) {
+        toggleDarkMode();
+    }
+}
+function clickDarkBtn() {
+  const changeThemeBtn = document.querySelector("#change-theme");
+
+  changeThemeBtn.addEventListener("change", function () {
+    toggleDarkMode();
+
+    // Save or remove dark mode
+    localStorage.removeItem("dark");
+
+    if(document.body.classList.contains("dark")) {
+        localStorage.setItem("dark", 1);
+    }
+});
+}
+
+
