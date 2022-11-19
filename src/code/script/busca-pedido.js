338a0fb48ddd5979a59029caba8265b1645bf494
LucasCNS <celionuvem@gmail.com>
2022-11-19 17:22:46 -0300

    DEV - Acrescentado Login no menu NAV.

--- src/code/script/busca-pedido.js
+++ src/code/script/busca-pedido.js
@@ -1,6 +1,6 @@
 if(localStorage.getItem("token") == null){
 	alert("Para acessar esta página, você precisa entrar em uma conta.")
-	window.location.href = "./login.html"
+	window.location.href = "./login-transp.html"
 	document.getElementById("abaCadastrar").click();
 }
 
