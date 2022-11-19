76c3897510ca44d8c756ca5d4c6793f2033cff93
dieguinsharp <diegoomaispika@outlook.com>
2022-11-18 23:12:57 -0300

    Added endpoint pra retornar o nome do cliente pelo idPedido.

--- src/StradeAPI/API/Controllers/PedidoController.cs
+++ src/StradeAPI/API/Controllers/PedidoController.cs
@@ -86,5 +86,26 @@ namespace API.Controllers {
             return NotFound("Pedido inexistente.");
         }
 
+        [HttpGet]
+        [Route("pedido/{idPedido}/cliente")]
+        public async Task<ActionResult<string>> GetNomeClienteByIdPedido([FromServices] DataContext context, [FromRoute] int idPedido){
+
+            if(idPedido == 0)
+                return NotFound("Id do pedido inválido.");
+
+            var nomeCliente = await (from p in context.Pedidos
+                                join c in context.Clientes on p.IdCliente equals c.IdCliente
+                                join i in context.Informacaos on c.IdInformacao equals i.IdInformacao
+                                where p.IdPedido == idPedido
+                                select i.Nome).FirstOrDefaultAsync(); 
+
+            if(!string.IsNullOrEmpty(nomeCliente)) {
+                return Ok(nomeCliente);
+            }
+                
+
+            return NotFound("Pedido/Cliente não encontrado");
+        }
+
     }
 }
